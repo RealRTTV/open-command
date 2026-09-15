53dcbc748e8d9961fe8fb8d42e4e3a5d4799100e
riley <tristanandlucky@gmail.com>
2026-09-15 11:44:22 -0400

    patch

--- src/sample_frames.py
+++ src/sample_frames.py
@@ -43,7 +43,7 @@ def get_row_for_sample(date: str, hardcoded_play_id: Optional[str] = None) -> Op
         row_data = df[df["playId"] == hardcoded_play_id].iloc[0]
     else:
         rows: int = df.shape[0]
-        while _ in range(rows):
+        while _ in range(100):
             row: int = random.randint(0, rows - 1)
             row_data = df.iloc[row]
 
